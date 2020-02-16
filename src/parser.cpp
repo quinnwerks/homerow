479e496bbc7962dea5659b9faa723142d7159895
quinnwerks <quinn.smith98@gmail.com>
2020-02-16 14:14:25 -0500

    Notes / rough draft of parse tree

--- src/parser.cpp
+++ src/parser.cpp
@@ -15,10 +15,46 @@ Parser& Parser::operator=(Parser& copy_this) {
     return *this;    
 }
 
-bool Parser::processWord(TOKEN_PAIR word) {
+PARSER_RET_CODE Parser::validExpr(TOKEN_PAIR word) {
     return false;
 }
 
 std::string Parser::dumpRepresentation() {
     return "";
+}
+
+
+// This determines if a sentence is valid
+// If a control statement is detected validity
+// will be determined from the top down
+// Things to build: -> AST
+//                  -> CFG
+
+// Words -> Sentences as AST
+// AST -> CALL Graph
+Parser::PARSER_RET_CODE dummy_func(TOKEN_PAIR pair) {
+    auto token = pair.first;
+    if(token == LANG::TOKEN::END_FILE) {
+        return ACCEPT;
+    }
+    if(token == LANG::TOKEN::WHILE || token == LANG::TOKEN::IF) {
+        //TODO: Mutual recursion???
+    }
+    else if (token == LANG::TOKEN::EXPR_BEGIN || token == LANG::EXPR_END) {
+        return FAIL;
+    }
+    else if (token == LANG::TOKEN::UNKNOWN){
+        return FAIL;
+    }
+    else {
+        return dummy_func(getNextPair)
+    }
+
+}
+
+Parser::PARSER_RET_CODE while_dummy_func(TOKEN_PAIR pair) {
+    auto token = pair.first;
+    if(token == LANG::TOKEN::EXPR_BEGIN) {
+
+    }
 }
--- src/parser.h
+++ src/parser.h
@@ -1,14 +1,23 @@
 #include "common.h"
+#include "scanner.h"
 
 #ifndef PARSER_H
 #define PARSER_H
 class Parser {
     public:
+        enum PARSER_RET_CODE {
+            ACCEPT,
+            FAIL
+        };
         Parser();
         Parser(Parser& copy_this);
         Parser& operator=(Parser & copy_this);
 
-        bool processWord(TOKEN_PAIR word);
-        std::string dumpRepresentation();    
+
+        PARSER_RET_CODE validExpr(TOKEN_PAIR word);
+        std::string dumpRepresentation();   
+        
+    private:
+        Scanner m_scanner;
 };
 #endif
