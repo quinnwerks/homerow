9af8d0f4943e370284c627c1c1b35bec626608fc
quinnwerks <quinn.smith98@gmail.com>
2020-04-18 21:39:41 -0400

    add skeleton code for basic ops

--- src/parser.cpp
+++ src/parser.cpp
@@ -13,22 +13,47 @@ Parser& Parser::operator=(Parser& copy_this) {
 
 
 /// @brief Entry point for parser
-PARSER_RET_CODE Parser::parse_token(TOKEN_PAIR pair) {
-    auto token = pair.first;
+PARSER_RET_CODE Parser::parse_token(const TOKEN_PAIR& token_pair) {
+    auto ret_code = PARSER_RET_CODE::FAIL;
+    auto token = token_pair.first;
     switch(token) {
-        case TOKEN::UP: break;
-        case TOKEN::DOWN: break;
-        case TOKEN::LEFT: break;
-        case TOKEN::RIGHT: break;
-        case TOKEN::IN: break;
+        // PERFROM OP ON CURRENT CELL
+        case TOKEN::UP: 
+        case TOKEN::DOWN:  ret_code = parse_cell_oper(token_pair);
+                           break;
+
+        // CHANGE CELL
+        case TOKEN::LEFT:
+        case TOKEN::RIGHT: ret_code = parse_cell_move(token_pair);
+                           break;
+        
+        // OUTPUT CELL
+        case TOKEN::IN: 
         case TOKEN::OUT: break;
-        case TOKEN::IF: break;
-        case TOKEN::WHILE: break;
+        
+        // FLOW CONTROL
+        case TOKEN::IF: 
+        case TOKEN::WHILE: 
+
+        // OTHER...
         case TOKEN::EXPR_BEGIN: break;
         case TOKEN::EXPR_END: break;
-        case TOKEN::UNKNOWN: break;
+
+        // EOF
         case TOKEN::END_FILE: break;
         default: break; 
     }
+    
+    return ret_code;
+}
 
+/// @brief Create an increment or decrement node
+PARSER_RET_CODE Parser::parse_cell_oper(const TOKEN_PAIR& token_pair) {
+    return PARSER_RET_CODE::CONTINUE_OPER;
+}
+
+/// @brief Move up a cell or down a cell. If the cell does not exist
+///        create one.
+PARSER_RET_CODE Parser::parse_cell_move(const TOKEN_PAIR& token_pair) {
+    return PARSER_RET_CODE::CONTINUE_MOVE;
 }
--- src/parser.h
+++ src/parser.h
@@ -4,6 +4,8 @@
 #ifndef PARSER_H
 #define PARSER_H
 enum class PARSER_RET_CODE {
+    CONTINUE_OPER,
+    CONTINUE_MOVE,
     ACCEPT,
     FAIL
 };
@@ -16,9 +18,11 @@ class Parser {
         Parser& operator=(Parser & copy_this);
 
         PARSER_RET_CODE parse(Scanner& scanner);
-        PARSER_RET_CODE parse_token(TOKEN_PAIR pair);
+        PARSER_RET_CODE parse_token(const TOKEN_PAIR& token_pair);
     
     private:
+        PARSER_RET_CODE parse_cell_oper(const TOKEN_PAIR& token_pair);
+        PARSER_RET_CODE parse_cell_move(const TOKEN_PAIR& token_pair);
         PARSER_RET_CODE validExpr(TOKEN_PAIR word);
         std::string dumpRepresentation();   
 };
