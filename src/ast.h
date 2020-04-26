8cc71fcf3eb6d26b90bf3f3ed35df724134ecd23
quinnwerks <quinn.smith98@gmail.com>
2020-04-26 09:37:36 -0400

    Add more AST functions

--- src/ast.h
+++ src/ast.h
@@ -7,8 +7,33 @@ class Ast {
     public:
         Ast();
         ~Ast();
+
+        // Interface to build the initial AST.
+        void insert(AstNode& new_node) {m_curr_expr->insertChild(new_node);}
+        void setCurrExpr(ExprNode& curr_expr) {m_curr_expr = &curr_expr;}
+        ExprNode& getCurrExpr() {return *m_curr_expr;}
+
+        //TODO: this should be done later
+        class iterator {
+            public:  
+                iterator(AstNode* ptr) : m_ptr(ptr) {}
+                bool operator==(const iterator& rhs) {return m_ptr == rhs.m_ptr; };
+                bool operator!=(const iterator& rhs) {return m_ptr != rhs.m_ptr; };
+
+                iterator operator++();
+                iterator operator--();
+                iterator operator++(int junk);
+                iterator operator--(int junk);
+                AstNode& operator*();
+                AstNode* operator->();
+            
+            private:
+                AstNode* m_ptr;
+        };
+
     private:
         ExprNode* m_root;
+        ExprNode* m_curr_expr;
 };
 
 #endif
