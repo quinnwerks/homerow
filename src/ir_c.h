25fb926c17de9e323f64395159ebed862338ed98
quinnwerks <quinn.smith98@gmail.com>
2020-05-20 17:06:42 -0400

    make header for ir to c

--- src/ir_c.h
+++ src/ir_c.h
@@ -1,4 +1,12 @@
 #ifndef IR_C_H
 #define IR_C_H
 
+class IrToC {
+    public:
+        std::string getOutput(const Ast& ast);
+    private:
+        std::string getExprString(const ExprNode& node);
+        std::string getBinaryString(const BinaryNode& node);
+};
+
 #endif
